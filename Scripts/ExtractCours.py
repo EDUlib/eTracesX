e9a570d71508272b958d4aa08ddc695d4200b7b5
Nicolas Leduc <hexabit@gmail.com>
2017-01-31 09:02:21 -0500

    Ajout de scripts de pretraitement des logs
    - Documentation du pipeline a executer pour obtenir une MOOCdb dans MySQL
    - Script d'extration et d'anonimisation des traces pour un cours
    - Script d'extraction d'un sous ensemble des traces pour les tests
    - Deplacement du script creant les DB MOOCdb dans Mysql

---
+++ Scripts/ExtractCours.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import os
+import sys
+import getopt
+import re
+import random
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print 'Input file is :', inputfile
+    print 'Output file is :', outputfile
+    if inputfile == '' or outputfile == '':
+        sys.exit()
+    pUser =re.compile('"username": "([\w.@&\-]*)"')
+    pCours =re.compile('ITES\.1')
+    nameDict = dict()
+    f = open(inputfile, "r")
+    copy = open(outputfile, "w")
+    for line in f:
+        mCours = pCours.search(line)
+        if mCours:
+            mUser = pUser.findall(line)
+            newLine = ''
+            if len(mUser) == 1:
+                if mUser[0] != '':
+                    if not nameDict.has_key(mUser[0]):
+                        newName = ''.join(random.SystemRandom().choice('0123456789ABCDEF') for _ in range(16))
+                        i = 0;
+                        while (newName in nameDict.values()) and i < 1000:
+                            newName = ''.join(random.SystemRandom().choice('0123456789ABCDEF') for _ in range(16))
+                            i = i+1;
+                        if i == 1000:
+                            print "Can't find a name :", mUser[0]
+                            sys.exit()
+                        nameDict[mUser[0]] = newName; 
+#                        print 'Username is :', mUser[0], ' ---  newName :', nameDict[mUser[0]]
+                    newLine = re.sub('"username": "'+ mUser[0] + '"', '"username": "' + nameDict[mUser[0]] + '"', line)
+#                    newLine = re.sub('"username": "'+ mUser[0] + '"', '"username": "' + mUser[0] + '"', line)
+#                    newLine = line
+                else:
+                    newLine = line
+            else:
+                print line
+                sys.exit()
+            if newLine != '':
+                copy.write(newLine)
+    f.close()
+    copy.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
---
+++ Scripts/ExtractSubSet.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import re
+import datetime
+import json
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print 'Input file is :', inputfile
+    print 'Output file is :', outputfile
+    if inputfile == '' or outputfile == '':
+        sys.exit()
+
+    nameDict = dict()
+
+    f = open(inputfile, "r")
+    for line in f:
+        try:
+            record = json.loads(str(line))
+        except ValueError as e:
+            continue
+        if record['username'] == '':
+            continue
+        eventTimeStr = record['time']
+        maybeOffsetDir = eventTimeStr[-6]
+        if maybeOffsetDir == '+' or maybeOffsetDir == '-': 
+            eventTimeStr = eventTimeStr[0:-6]
+        eventDate = datetime.datetime.strptime(eventTimeStr[0:10], '%Y-%m-%d')
+        #.dateT%H:%M:%S.%f').date
+        if not nameDict.has_key(record['username']):
+            nameDict[record['username']]=(record['username'],eventDate,0)
+        lastEventDate = eventDate
+        if nameDict[record['username']][1]>eventDate:
+            lastEventDate = nameDict[record['username']][1]
+        nameDict[record['username']] = (nameDict[record['username']][0],lastEventDate,nameDict[record['username']][2]+1)
+    f.close()
+    dateDict = dict()
+    for aTuple in nameDict.values():
+        if not dateDict.has_key(aTuple[1]):
+            dateDict[aTuple[1]] = []
+        dateDict[aTuple[1]].append(aTuple)
+    filterNameSet = set()
+    for tupleList in dateDict.values():
+        tupleList.sort(key=lambda tup: tup[2])
+        if len(tupleList) < 3:
+            for aTuple in tupleList:
+                filterNameSet.add(aTuple[0])
+        else:
+            filterNameSet.add(tupleList[0][0])
+            filterNameSet.add(tupleList[len(tupleList)-1][0])
+            filterNameSet.add(tupleList[(len(tupleList)-1)/2][0])
+    print filterNameSet
+    print str(len(filterNameSet)) + " : " + str(len(dateDict)) + " : " + str(len(nameDict))
+    f = open(inputfile, "r")
+    copy = open(outputfile, "w")
+    for line in f:
+        try:
+            record = json.loads(str(line))
+        except ValueError as e:
+            copy.write(line)
+            continue
+        if record['username'] == '' or record['username'] in filterNameSet:
+            copy.write(line)
+    copy.close()
+    f.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
