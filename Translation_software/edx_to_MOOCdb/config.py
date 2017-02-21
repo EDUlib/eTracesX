486b30f91f80c89f9940ede0e7541f19ed0080e5
Nicolas Leduc <nicolas.leduc@polymtl.ca>
2017-02-21 11:28:06 -0500

    Ajout et mise a jour de la documentation du pipeline MOOCdb

--- Translation_software/edx_to_MOOCdb/config.py
+++ Translation_software/edx_to_MOOCdb/config.py
@@ -1,6 +1,11 @@
 # Configuration file
 import re
 
+## Input file
+EDX_TRACK_EVENT_LOG = '/Users/leducni/Documents/DATA/TEST-BUGGY/logs-test-ITES.txt'
+## Output files
+DEST_DIR = '/Users/leducni/Documents/DATA/TEST-BUGGY/MOOCDB/'
+
 ## Input source
 ## Can be either 'csv', 'sql' or 'json'
 INPUT_SOURCE = 'json'
@@ -9,37 +14,9 @@ TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
 # MIT courses : %Y-%m-%dT%H:%M:%S.%f
 # Stanford Courses : %Y-%m-%d %H:%M:%S
 
-
-## CSV input files
-COURSE_NAME='dalmooc'
-
-CSV_SOURCE_DIR = '/Users/leducni/Documents/DATA/TEST-ITES/DB/'
-CSV_PREFIX = 'logs-test-ULB.txt.2017-01-30T16_11_36.197853_20240.sql'
-
-EDX_TRACK_EVENT = ''.join([CSV_SOURCE_DIR, CSV_PREFIX, '_EdxTrackEventTable.csv'])
-CORRECT_MAP = ''.join([CSV_SOURCE_DIR, CSV_PREFIX, '_CorrectMapTable.csv'])
-ANSWER = ''.join([CSV_SOURCE_DIR, CSV_PREFIX, '_AnswerTable.csv'])
-
-## Output files
-DEST_DIR = '/Users/leducni/Documents/DATA/TEST-BUGGY/MOOCDB_NEW/'
-EDX_TRACK_EVENT_LOG = '/Users/leducni/Documents/DATA/TEST-BUGGY/logs-test-ITES.txt'
-
 ### Hierarchy pretty prints
 RESOURCE_HIERARCHY = DEST_DIR + 'resource_hierarchy.org'
 PROBLEM_HIERARCHY = DEST_DIR + 'problem_hierarchy.org' 
-
-### MOOCdb CSV tables
-OBSERVED_EVENTS = DEST_DIR + 'observed_events.csv'
-RESOURCES = DEST_DIR + 'resources.csv'
-RESOURCES_URLS = DEST_DIR + 'resources_urls.csv'
-URLS = DEST_DIR + 'urls.csv'
-RESOURCE_TYPES = DEST_DIR + 'resource_types.csv'
-PROBLEMS = DEST_DIR + 'problems.csv'
-SUBMISSIONS = DEST_DIR + 'submissions.csv'
-ASSESSMENTS = DEST_DIR + 'assessments.csv'
-PROBLEM_TYPES = DEST_DIR + 'problem_types.csv'
-OS = DEST_DIR + 'os.csv'
-AGENT = DEST_DIR + 'agent.csv'
 
 ### Log file
 VERBOSE = True
@@ -59,5 +36,15 @@ MISSING_TRAILING_SLASH = '(?<!/)$'
 GET_DOMAIN = re.compile('(?P<domain>^.+://[^/]+)')
 COURSEWARE = re.compile('courseware/(?P<unit>[^/]+)?/?(?P<subunit>[^/]+)?/?(?P<seq>\d{1,2})?')
 BOOK = re.compile('book/(?P<booknum>\d{1,2})/(?P<page>\d{1,4})?')
+
+## Obsolete
+## CSV input files
+CSV_SOURCE_DIR = '/Users/leducni/Documents/DATA/TEST-ITES/DB/'
+CSV_PREFIX = 'logs-test-ULB.txt.2017-01-30T16_11_36.197853_20240.sql'
+
+EDX_TRACK_EVENT = ''.join([CSV_SOURCE_DIR, CSV_PREFIX, '_EdxTrackEventTable.csv'])
+CORRECT_MAP = ''.join([CSV_SOURCE_DIR, CSV_PREFIX, '_CorrectMapTable.csv'])
+ANSWER = ''.join([CSV_SOURCE_DIR, CSV_PREFIX, '_AnswerTable.csv'])
+
 
 
